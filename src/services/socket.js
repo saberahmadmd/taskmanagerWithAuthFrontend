2d5a72c42a5f6b29f7f2378a3da7325f87c46215
saberahmadmd <157378864+saberahmadmd@users.noreply.github.com>
2025-05-10 12:41:04 +0530

    Update socket.js

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,7 +1,13 @@
+import { io } from 'socket.io-client';
+
+let socket;
+
 export const initializeSocket = () => {
   if (!socket) {
     const socketUrl = process.env.REACT_APP_SOCKET_URL || 'http://localhost:5000';
-    socket = io(socketUrl);
+    socket = io(socketUrl, {
+      transports: ['websocket'],
+    });
   }
   return socket;
 };
